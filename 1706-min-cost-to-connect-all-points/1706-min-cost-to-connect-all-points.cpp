2b44fc693d807d86ba75ba2f97fb6a44891d1382
Nishant Tomer <97550493+NishantTomer5601@users.noreply.github.com>
2023-09-14 19:04:30 +0530

    Time: 2780 ms (5.01%), Space: 468 MB (5.04%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -1,51 +1,4 @@
-// class Solution {
-// public:
-//     int minCostConnectPoints(vector<vector<int>>& points) {
-//         vector<vector<pair<int, int>>> adj(points.size());
-//         vector<int> parent(points.size(), 0);
-//         vector<int> key(points.size(), INT_MAX);
-//         vector<int> mst(points.size(), 0);
-//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-//         int ans = 0;
-//         for (int i = 0; i < points.size(); ++i) {
-//             for (int j = 0; j < points.size(); ++j) {
-//                 if (i != j) {
-//                     adj[i].push_back({j, abs(points[i][1] - points[j][1]) + abs(points[i][0] - points[j][0])});
-//                 }
-//             }
-//         }
-//         pq.push({0, 0});
-//         key[0] = 0;
-//         while(!pq.empty()) {
-//             int u = pq.top().second;
-//             pq.pop();
-//             if (mst[u]) {
-//                 continue;
-//             }
-//             mst[u] = true;
-//             for (int i = 0; i < adj[u].size(); ++i) {
-//                 auto [v, w] = adj[u][i];
-//                 if (!mst[v] && key[v] > w) {
-//                     key[v] = w;
-//                     pq.push({key[v], v});
-//                     parent[v] = u;
-//                 }
-//             }
-//         }
-//         for (int i = 1; i < parent.size(); ++i) {
-//             for (int j = 0; j < adj[parent[i]].size(); ++j) {
-//                 if (adj[parent[i]][j].first == i) {
-//                     ans += adj[parent[i]][j].second;
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// };
 
-
-
-#define ll long long
 class Solution {
 private:
     
@@ -69,7 +22,7 @@ public:
         }
 
         vector<int> vis(n,0);
-        ll sum=0;
+        int sum=0;
 
         priority_queue<pair<int,int>,vector<pair<int,int>> ,greater<pair<int,int>>> pq;
         pq.push({0,0});
