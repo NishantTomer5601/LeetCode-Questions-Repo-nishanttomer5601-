1a59af5453dc42559904098b62c0b4d5d1595ccf
Nishant Tomer <97550493+NishantTomer5601@users.noreply.github.com>
2023-09-18 13:29:52 +0530

    Time: 56 ms (95.99%), Space: 59.4 MB (6.85%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n=height.size();
+        int i=0,j=n-1;
+        int ans=INT_MIN;
+        while(i<j){
+            
+            ans=max(ans,min(height[i],height[j])*(j-i));
+            height[i]>=height[j]?j--:i++;
+
+        }
+
+        return ans;
+        
+    }
+};
