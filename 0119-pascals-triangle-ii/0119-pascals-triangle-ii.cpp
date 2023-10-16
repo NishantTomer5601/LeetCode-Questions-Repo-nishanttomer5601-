f6eb211e0fd7d9d15b07b6c3bd9a98cf5bdad945
Nishant Tomer <97550493+NishantTomer5601@users.noreply.github.com>
2023-10-16 11:51:06 +0530

    Time: 3 ms (36.93%), Space: 6.8 MB (35.86%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,16 +1,30 @@
-
 class Solution {
 public:
+    int combi(int n, int k){
+        long long int res = 1;
+ 
+    // Since C(n, k) = C(n, n-k)
+    if (k > n - k)
+        k = n - k;
+ 
+    // Calculate value of
+    // [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1]
+    for (int i = 0; i < k; ++i) {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+ 
+    return res;
+    }
     vector<int> getRow(int rowIndex) {
-        vector<int> result(rowIndex + 1);
-        for (int i = 0; i < result.size(); ++i) {
-            int prev_result = result[0] = 1;
-            for (int j = 1; j <= i; ++j) {
-                const int tmp = result[j];
-                result[j] += prev_result;
-                prev_result = tmp;
-            }
+        int n = rowIndex;
+        vector<int> res;
+        int r = 0;
+        while(r <= rowIndex){
+            int x = combi(n, r);
+            res.push_back(x);
+            r++;
         }
-        return result;
+        return res;
     }
 };
