9ec0224ad98e50ef0e6af0379549c0f502369be0
Nishant Tomer <97550493+NishantTomer5601@users.noreply.github.com>
2023-11-06 22:31:28 +0530

    Time: 309 ms (53.95%), Space: 148.1 MB (41.61%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
@@ -1,6 +1,6 @@
 class SeatManager {
 private:
-    std::priority_queue<int, std::vector<int>, std::greater<int>> seats; // Declare a min-heap to manage seat reservations.
+    priority_queue<int, vector<int>, greater<int>> seats; // Declare a min-heap to manage seat reservations.
 
 public:
     // Constructor to initialize the SeatManager with 'n' seats.
@@ -17,9 +17,8 @@ public:
             int reservedSeat = seats.top(); // Get the smallest seat number from the heap.
             seats.pop(); // Remove the reserved seat from the heap.
             return reservedSeat; // Return the reserved seat number.
-        } else {
-            return -1; // Return -1 to indicate that there are no available seats.
-        }
+        } 
+        return 0;
     }
 
     // Unreserve a seat.
