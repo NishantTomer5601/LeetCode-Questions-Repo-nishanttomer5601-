c74b1889a93895bdd23ab65e90cd8df15dfc5d68
Nishant Tomer <97550493+NishantTomer5601@users.noreply.github.com>
2023-11-14 23:51:01 +0530

    Time: 241 ms (78.77%), Space: 13.2 MB (80.37%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+class Solution {
+public:
+    int countPalindromicSubsequence(std::string s) {
+        int count = 0;
+        for (int i = 0; i < 26; ++i) {
+            char currentChar = static_cast<char>(i + 97);
+            int l = s.find(currentChar);
+            int r = s.rfind(currentChar);
+            if (l != -1 && r != -1 && l < r) {
+                count += std::unordered_set<char>(s.begin() + l + 1, s.begin() + r).size();
+            }
+        }
+        return count;
+    }
+};
