bdf727e55421237b7c636d70f709f0607c8d1974
Nishant Tomer <97550493+NishantTomer5601@users.noreply.github.com>
2024-01-22 16:46:26 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (6.32%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
-        
+      return log2(std::pow(2, a) * std::pow(2, b));
+        //return log(exp(a))+log(exp(b));
     }
 };
