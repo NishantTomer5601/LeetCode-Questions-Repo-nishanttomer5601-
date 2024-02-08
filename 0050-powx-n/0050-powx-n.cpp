8851f11534461ee0dbd787bb9de02ddaca458404
Nishant Tomer <97550493+NishantTomer5601@users.noreply.github.com>
2024-02-08 19:50:56 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (5.23%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans=0;
-        ans=pow(x,n);
-        return ans;
+        // double ans=0;
+        // ans=;
+        return pow(x,n);
         
         
     }
