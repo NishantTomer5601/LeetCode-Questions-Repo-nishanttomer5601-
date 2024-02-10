204b8b31b907012ae758b9ef903843a102dad0bd
Nishant Tomer <97550493+NishantTomer5601@users.noreply.github.com>
2024-02-10 12:59:22 +0530

    Time: 27 ms (24.97%), Space: 14.8 MB (11.47%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,36 +2,90 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n=s.length();
-        if(n==0) return 0;
         unordered_map<char,int> map;
         int ans=INT_MIN;
         queue<char> q;
-        int streak=0;
+        if(n==0) return 0;
+        
         for(int i=0;i<n;i++){
-            char c=s[i];
-            if(map[s[i]]==0){
-                q.push(s[i]);
+            if(map.find(s[i])==map.end()){
                 map[s[i]]++;
-                streak++;               
-                ans=max(streak,ans);
-
+                 q.push(s[i]);
+                int x=map.size();
+                ans=max(ans,x);
+               
+                
             }
             else{
-                while(!q.empty() && map[c]!=0){
-                   map[q.front()]--;
+                
+                while(!q.empty() && q.front()!=s[i]){
+                    map[q.front()]--;
+                    if(map[q.front()]==0)
+                    map.erase(q.front());
                     q.pop();
-
                 }
-                map[c]++;
-                q.push(c);
-                streak=q.size();
-                ans=max(streak,ans);
-              
-
+                 map[q.front()]--;
+                    if(map[q.front()]==0)
+                    map.erase(q.front());
+                q.pop();
+                q.push(s[i]);
+                map[s[i]]++;
+                int x=map.size();
+                ans=max(ans,x);
+                
+                
+                
             }
         }
-
+        int x=map.size();
+        ans=max(ans,x);
         return ans;
-        
     }
 };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n=s.length();
+//         if(n==0) return 0;
+//         unordered_map<char,int> map;
+//         int ans=INT_MIN;
+//         queue<char> q;
+//         int streak=0;
+//         for(int i=0;i<n;i++){
+//             char c=s[i];
+//             if(map[s[i]]==0){
+//                 q.push(s[i]);
+//                 map[s[i]]++;
+//                 streak++;               
+//                 ans=max(streak,ans);
+
+//             }
+//             else{
+//                 while(!q.empty() && map[c]!=0){
+//                    map[q.front()]--;
+//                     q.pop();
+
+//                 }
+//                 map[c]++;
+//                 q.push(c);
+//                 streak=q.size();
+//                 ans=max(streak,ans);
+              
+
+//             }
+//         }
+
+//         return ans;
+        
+//     }
+// };
+
+
+
+
